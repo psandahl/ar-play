a2a31751dcdac67c7a724bef0075d18e3d1a4509
Patrik Sandahl <patrik.sandahl@gmail.com>
2020-10-18 19:59:53 +0200

    Add center of projection to transformer.

--- src/Transformer.cpp
+++ src/Transformer.cpp
@@ -55,6 +55,13 @@ void Transformer::projectPoints(const std::vector<cv::Point3d>& objectPoints,
   }
 }
 
+cv::Point3d Transformer::centerOfProjection() const
+{
+  cv::Mat m = rot().t() * trans().mul(-1.0);
+
+  return { m.at<double>(0), m.at<double>(1), m.at<double>(2) };
+}
+
 cv::Mat Transformer::makeE(const cv::Mat& rvec, const cv::Mat& tvec) const
 {
   cv::Mat rmat;
@@ -78,4 +85,34 @@ cv::Mat Transformer::makeE(const cv::Mat& rvec, const cv::Mat& tvec) const
   E.at<double>(2, 3) = tvec.at<double>(2);
 
   return E;
+}
+
+cv::Mat Transformer::rot() const
+{
+  cv::Mat r(3, 3, CV_64FC1);
+
+  r.at<double>(0, 0) = _E.at<double>(0, 0);
+  r.at<double>(1, 0) = _E.at<double>(1, 0);
+  r.at<double>(2, 0) = _E.at<double>(2, 0);
+
+  r.at<double>(0, 1) = _E.at<double>(0, 1);
+  r.at<double>(1, 1) = _E.at<double>(1, 1);
+  r.at<double>(2, 1) = _E.at<double>(2, 1);
+
+  r.at<double>(0, 2) = _E.at<double>(0, 2);
+  r.at<double>(1, 2) = _E.at<double>(1, 2);
+  r.at<double>(2, 2) = _E.at<double>(2, 2);
+
+  return r;
+}
+
+cv::Mat Transformer::trans() const
+{
+  cv::Mat t(3, 1, CV_64FC1);
+
+  t.at<double>(0) = _E.at<double>(0, 3);
+  t.at<double>(1) = _E.at<double>(1, 3);
+  t.at<double>(2) = _E.at<double>(2, 3);
+
+  return t;
 }
--- src/Transformer.h
+++ src/Transformer.h
@@ -20,9 +20,14 @@ public:
   // Project a set of object points to image points.
   void projectPoints(const std::vector<cv::Point3d>& objectPoints, std::vector<cv::Point2d>& imagePoints) const;
 
+  // Get the center of projection.
+  cv::Point3d centerOfProjection() const;
+
 private:
 
   cv::Mat makeE(const cv::Mat& rvec, const cv::Mat& tvec) const;
+  cv::Mat rot() const;
+  cv::Mat trans() const;
 
   cv::Mat _K;
   cv::Mat _E;
